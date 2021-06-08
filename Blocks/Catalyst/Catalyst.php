d35b37b5fd716870460fc45ed78db2997d0f68f0
Maxim <maximakimov3@gmail.com>
2021-06-08 12:11:01 +0300

    update

--- Blocks/Catalyst/Catalyst.php
+++ Blocks/Catalyst/Catalyst.php
@@ -5,10 +5,8 @@ namespace WpThemeBones\Blocks\Catalyst;
 defined('ABSPATH') ||
 die('Constant missing');
 
-use WpThemeBones\Blocks\Common\Common;
 use WpThemeBones\Classes\Block;
 
 class Catalyst extends Block
 {
-    protected Common $common;
 }
--- Blocks/DemoPage/DemoPage.php
+++ Blocks/DemoPage/DemoPage.php
@@ -9,13 +9,11 @@ use LightSource\FrontBlocks\Helper;
 use LightSource\FrontBlocks\Settings;
 use WpThemeBones\Blocks\Article\Theme\Classic\ArticleThemeClassic;
 use WpThemeBones\Blocks\Catalyst\Catalyst;
-use WpThemeBones\Blocks\Common\Common;
 use WpThemeBones\Classes\Block;
 
 class DemoPage extends Block
 {
 
-    protected Common $common;
     protected Catalyst $catalyst;
     protected ArticleThemeClassic $article;
 
