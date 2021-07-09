33e94ac6037b378e8a6e98c05760f43638aab6b0
Maxim <maximakimov3@gmail.com>
2021-07-09 15:07:13 +0300

    update laravel-mix (webpack)

--- node-js/webpack.mix.js
+++ node-js/webpack.mix.js
@@ -1,92 +1,95 @@
-let laravelMix = require('laravel-mix');
-let glob = require('glob');
-let path = require("path");
+let laravelMix = require('laravel-mix')
+let glob = require('glob')
+let path = require('path')
 
 class Mix {
 
-    constructor(blocksRelativePath) {
+  constructor (blocksRelativePath) {
 
-        this._scssFiles = [];
-        this._jsFiles = [];
-        this._blocksRelativePath = blocksRelativePath;
-        this._blocksAbsPath = path.resolve(__dirname + '/' + this._blocksRelativePath);
+    this.scssFiles = []
+    this.tsFiles = []
+    this.blocksRelativePath = blocksRelativePath
+    this.blocksAbsPath = path.resolve(__dirname + '/' + this.blocksRelativePath)
 
-        this._setupMix();
-        this._readFiles();
-        this._mix();
+    this.setupMix()
+    this.readFiles()
+    this.mix()
 
-    }
+  }
 
-    _setupMix() {
+  setupMix () {
 
-        laravelMix.setPublicPath(this._blocksRelativePath);
-        laravelMix.webpackConfig({
-            resolve: {
-                modules: [
-                    __dirname + '/node_modules'
-                ],
-            },
-        });
+    laravelMix.setPublicPath(this.blocksRelativePath)
+    laravelMix.webpackConfig({
+      resolve: {
+        modules: [
+          __dirname + '/node_modules',
+        ],
+      },
+    })
 
-        laravelMix.options({
-            terser: {
-                terserOptions: {
-                    // class names is required for Catalyst
-                    keep_classnames: /.*Element/,
-                    keep_fnames: /.*Element/,
-                },
-            },
-        });
+    laravelMix.options({
+      terser: {
+        terserOptions: {
+          // class names is required for Catalyst
+          keep_classnames: /.*Element/,
+          keep_fnames: /.*Element/,
+        },
+      },
+    })
 
-        // makes babel is dependent on package.json browserslist, so will skip additional polyfills
-        laravelMix.babelConfig({
-            "presets": [
-                [
-                    "@babel/preset-env",
-                    {}
-                ]
-            ]
-        });
+    // makes babel is dependent on package.json browserslist, so will skip additional polyfills
+    laravelMix.babelConfig({
+      'presets': [
+        [
+          '@babel/preset-env',
+          {},
+        ],
+      ],
+    })
 
-    }
+  }
 
-    _readFiles() {
+  readFiles () {
 
-        this._scssFiles = glob.sync(this._blocksAbsPath + "/**/*.scss");
-        this._jsFiles = glob.sync(this._blocksAbsPath + "/**/*.ts");
-        /*   .filter((jsFileName) => {
-           return (!jsFileName.match('\.min\.js') &&
-               !jsFileName.match('\.json'));
-       });*/
+    this.scssFiles = glob.sync(this.blocksAbsPath + '/**/*.scss')
+    this.tsFiles = glob.sync(this.blocksAbsPath + '/**/*.ts')
+    /*   .filter((jsFileName) => {
+       return (!jsFileName.match('\.min\.js') &&
+           !jsFileName.match('\.json'));
+   });*/
 
-    }
+  }
 
-    _mix() {
+  mix () {
 
-        this._scssFiles.forEach((absPathToScssFile, index) => {
+    // will contain a list of dependencies
+    laravelMix.js(this.blocksAbsPath + '/stub.js', this.blocksAbsPath + '/stub.min.js')
 
-            let absPathToMinCssFile = absPathToScssFile.replace('.scss', '.min.css');
-            laravelMix.sass(absPathToScssFile, absPathToMinCssFile).options({
-                autoprefixer: {
-                    options: {
-                        browsers: [
-                            'last 2 versions',
-                        ],
-                    },
-                },
-            });
+    this.scssFiles.forEach((absPathToScssFile, index) => {
 
-        });
+      let absPathToMinCssFile = absPathToScssFile.replace('.scss', '.min.css')
+      laravelMix.sass(absPathToScssFile, absPathToMinCssFile).options({
+        autoprefixer: {
+          options: {
+            browsers: [
+              'last 3 versions',
+            ],
+          },
+        },
+      })
 
-        this._jsFiles.forEach((absPathToJsFile, index) => {
+    })
 
-            let absPathToMinJsFile = absPathToJsFile.replace('.ts', '.min.js');
-            laravelMix.ts(absPathToJsFile, absPathToMinJsFile);
+    this.tsFiles.forEach((absPathToTsFile, index) => {
 
-        });
+      let absPathToMinJsFile = absPathToTsFile.replace('.ts', '.min.js')
+      laravelMix.ts(absPathToTsFile, absPathToMinJsFile)
 
-    }
+    })
+
+  }
 
 }
 
-new Mix("../Blocks");
+new Mix('../Blocks')
